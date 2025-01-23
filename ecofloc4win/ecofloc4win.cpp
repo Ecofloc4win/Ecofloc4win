fb67da4cc0b34b8b935a71e35f439a5ecd827c02
Mattis Pingard <mattis.pingard@gmail.com>
2025-01-23 10:21:06 +0100

    [+] disable command

--- ecofloc4win/ecofloc4win.cpp
+++ ecofloc4win/ecofloc4win.cpp
@@ -62,7 +62,7 @@ void removeProcByLineNumber(const string&) noexcept;
 void removeProcPid(string, string);
 
 void enable(const string&, const string&);
-void disable(string);
+void disable(const string&, const string&);
 
 std::wstring getLocalizedCounterPath(const std::wstring& processName, const std::string& counterName);
 
@@ -634,8 +634,6 @@ int main()
 
 										double averagePower = downloadPower + uploadPower;
 
-										double intervalEnergy = averagePower * intervalSec;
-
 										{
 											std::lock_guard<std::mutex> lock(data_mutex);
 											// Update the NIC energy for the process
@@ -645,7 +643,7 @@ int main()
 												});
 											if (it != monitoringData.end())
 											{
-												it->updateNICEnergy(intervalEnergy);
+												it->updateNICEnergy(averagePower);
 											}
 										}
 									}
@@ -786,9 +784,9 @@ void readCommand(string commandHandle)
 		break;
 
 	case 2://to do
-		if (chain.size() == 2)
+		if (chain.size() == 3)
 		{
-			disable(chain[1]);
+			disable(chain[1], chain[2]);
 		}
 		else
 		{
@@ -1202,16 +1200,69 @@ void enable(const string& lineNumber, const string& component)
 	}
 }
 
-void disable(string component)
+void disable(const string& lineNumber, const string& component)
 {
-	if (comp[component].second)
-	{
-		comp[component].second = false;
-		cout << component << " is now disable" << endl;
+	enum ComponentType { CPU, GPU, SD, NIC };
+	std::unordered_map<std::string, ComponentType> comp = {
+		{ "CPU", ComponentType::CPU },
+		{ "GPU", ComponentType::GPU },
+		{ "SD", ComponentType::SD },
+		{ "NIC", ComponentType::NIC }
+	};
+
+	try {
+		int line = std::stoi(lineNumber);
+		if (line < 0)
+		{
+			std::cerr << "Error: Line number cannot be negative." << std::endl;
+			return;
+		}
+
+		std::unique_lock<std::mutex> lock(data_mutex);
+		if (line >= monitoringData.size())
+		{
+			std::cerr << "Error: Line number is out of range." << std::endl;
+			return;
+		}
+
+		auto& data = monitoringData[line];
+		data.disableComponent(component);
+
+		auto it = comp.find(component);
+		if (it != comp.end())
+		{
+			it->second;
+		}
+
+		switch (it->second)
+		{
+		case CPU:
+			new_data_cpu.store(true, std::memory_order_release);
+			break;
+		case GPU:
+			new_data_gpu.store(true, std::memory_order_release);
+			break;
+		case SD:
+			new_data_sd.store(true, std::memory_order_release);
+			break;
+		case NIC:
+			new_data_nic.store(true, std::memory_order_release);
+			break;
+		}
+
+		std::cout << "Component " << component << " has been enabled for process " << data.getName() << " at line " << line << std::endl;
+
 	}
-	else
+	catch (const std::invalid_argument& e)
 	{
-		cout << component << " was already disable" << endl;
+		std::cerr << "Error: Invalid line number. Must be a number." << std::endl;
 	}
-	//to do disable component
+	catch (const std::out_of_range& e)
+	{
+		std::cerr << "Error: Line number is too large." << std::endl;
+	}
+	catch (...)
+	{
+		std::cerr << "Unexpected error during process removal." << std::endl;
+	}
 }

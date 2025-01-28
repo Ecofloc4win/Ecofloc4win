453d78259bf816f7fcaad557597852052bd59d7e
AlexisDumolie <a.dumolie32@gmail.com>
2025-01-28 14:54:32 +0100

    [~] Refactor of toCsv.js

--- ecofloc-UI/src/Js/toCsv.js
+++ ecofloc-UI/src/Js/toCsv.js
@@ -6,76 +6,91 @@ document.getElementById('exportCSV').addEventListener('click', async () => {
             throw new Error('graphs does not exists');
         }
 
-        let actualDateInSecond = new Date().getTime() / 1000;
-        let monitoringDuration = Math.max(
-                                ...Object.values(graphCPU.data)
-                                .flatMap(obj => obj.x)
-                            );
-        let monitoringStartTime = actualDateInSecond - monitoringDuration;
-
-        
+        monitoringStartTime = getStartMonitoringTime();
 
         // Collect data from all graphs with x and y values
-        const graphData = {
-        CPU: Object.fromEntries(
-            Object.entries(graphCPU.data).map(([pid, data]) => [pid,
-            {
-                // Ajuste chaque élément de la liste x pour inclure la date de début
-                x: data.x.map(timestamp => timestamp + monitoringStartTime) || [],
-                y: data.y || [],
-            },])),
-        GPU: Object.fromEntries(
-            Object.entries(graphGPU.data).map(([pid, data]) => [pid,
-            {
-                x: data.x.map(timestamp => timestamp + monitoringStartTime) || [],
-                y: data.y || [],
-            },])),
-        NIC: Object.fromEntries(
-            Object.entries(graphNIC.data).map(([pid, data]) => [pid,
-            {
-                x: data.x.map(timestamp => timestamp + monitoringStartTime) || [],
-                y: data.y || [],
-            },])),
-        SD: Object.fromEntries(
-            Object.entries(graphSD.data).map(([pid, data]) => [pid,
-            {
-                x: data.x.map(timestamp => timestamp + monitoringStartTime) || [],
-                y: data.y || [],
-            },]))
-        };
+        const graphData = recupGraphData(monitoringStartTime);
 
-        const response = await fetch('http://localhost:3030/export-csv', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(graphData)
-        });
+        blob = getCsv(graphData);
 
-        if (!response.ok) {
-            const errorText = await response.text();
-            console.error('Server error:', errorText);
-            throw new Error(`Export failed: ${errorText}`);
-        }
-        const blob = await response.blob();
-        console.log('Received blob:', blob);
+        launchDownload(blob);
 
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-
-        const now = new Date();
-        const date = now.toISOString().split('T')[0];
-        const time = `${now.getHours().toString().padStart(2, '0')}-${now.getMinutes().toString().padStart(2, '0')}`;
-        
-        a.download = `monitoring_data_${date}_${time}.csv`;
-        
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(url);
-        document.body.removeChild(a);
     } catch (error) {
         console.error('Error exporting data:', error);
         alert(`Erreur d'export : ${error.message}`);
     }
 });
+
+
+const recupGraphData = (startTime) => {
+    return {
+        CPU: Object.fromEntries(
+            Object.entries(graphCPU.data).map(([pid, data]) => [pid,
+            {
+                // Ajuste chaque élément de la liste x pour inclure la date de début
+                x: data.x.map(timestamp => timestamp + startTime) || [],
+                y: data.y || [],
+            },])),
+        GPU: Object.fromEntries(
+            Object.entries(graphGPU.data).map(([pid, data]) => [pid,
+            {
+                x: data.x.map(timestamp => timestamp + startTime) || [],
+                y: data.y || [],
+            },])),
+        NIC: Object.fromEntries(
+            Object.entries(graphNIC.data).map(([pid, data]) => [pid,
+            {
+                x: data.x.map(timestamp => timestamp + startTime) || [],
+                y: data.y || [],
+            },])),
+        SD: Object.fromEntries(
+            Object.entries(graphSD.data).map(([pid, data]) => [pid,
+            {
+                x: data.x.map(timestamp => timestamp + startTime) || [],
+                y: data.y || [],
+            },]))
+        };
+} 
+
+const launchDownload = (blob) => {
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+
+    const now = new Date();
+    const date = now.toISOString().split('T')[0];
+    const time = `${now.getHours().toString().padStart(2, '0')}-${now.getMinutes().toString().padStart(2, '0')}`;
+    
+    a.download = `monitoring_data_${date}_${time}.csv`;
+    
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+}
+
+const getStartMonitoringTime = () => {
+    let actualDateInSecond = new Date().getTime() / 1000;
+    let monitoringDuration = Math.max(
+                            ...Object.values(graphCPU.data)
+                            .flatMap(obj => obj.x)
+                        );
+    return actualDateInSecond - monitoringDuration;
+}
+
+async function getCsv(graph){
+    const response = await fetch('http://localhost:3030/export-csv', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(graph)
+    });
+
+    if (!response.ok) {
+        const errorText = await response.text();
+        console.error('Server error:', errorText);
+        throw new Error(`Export failed: ${errorText}`);
+    }
+    return await response.blob();
+}
